7e96fd6ac50464a38f10f706fb2550f362112447
arabarkh <arabarkh@usc.edu>
2024-11-11 18:02:50 -0800

    Updates

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 # CSCI578_Team15_CryptoBoard
 
-Packages installed so far:
+# 1. Packages installed so far:
+
+pip3 install requests
+pip3 install psycopg2
 pip3 install flask
 pip3 install bs4
+
+2. Create a .env file with the attributes below:
+
+# Database configuration
+DB_NAME=cryptoboard
+DB_USER=cryptoboard_owner
+DB_PASSWORD=PASSWORD
+DB_HOST=HOST_ADDRESS
+DB_SSLMODE=require
+DB_SSLROOTCERT=/path/to/root.crt
--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
-import requests
+from concurrent.futures import ThreadPoolExecutor
+import requests, os, psycopg2
 
 app = Flask(__name__)
+# multi threading 
+executor = ThreadPoolExecutor(max_workers=5)
+
+def loadEnvFile(filepath):
+    with open(filepath, 'r') as file:
+        for line in file:
+            if line.strip() and not line.startswith('#'):
+                key, value = line.strip().split('=', 1)
+                os.environ[key] = value
 
 def googleSearch(sourceName, cryptoName, numberOfResultsToCrawl):
     try:
@@ -19,15 +29,38 @@ def googleSearch(sourceName, cryptoName, numberOfResultsToCrawl):
         uniqueLinks = set() 
         links = []
         
+        loadEnvFile('.env')
+    
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            sslmode=os.getenv("DB_SSLMODE"),
+            sslrootcert=os.getenv("DB_SSLROOTCERT")
+        )
+        
+        cursor = conn.cursor()
+        
         for item in soup.find_all('a', href=True):
-            #print('\n' + str(item) + '\n')
             href = item['href']
             if href.startswith("https://")  and "google.com" not in href and "/search" not in href:
                 if href not in uniqueLinks:
-                    uniqueLinks.add(href)
-                    links.append(href)
+                    cursor.execute("SELECT 1 FROM STORED_URLS WHERE URL = %s;", (href,))
+                    queryResults = cursor.fetchone()
+                    if queryResults is None:
+                        uniqueLinks.add(href)
+                        links.append(href)
+                        try:
+                            cursor.execute("INSERT INTO STORED_URLS (CRYPTO_NAME, SOURCE, URL) VALUES (%s, %s, %s);", (cryptoName, sourceName, href))
+                            conn.commit()
+                        except Exception as e:
+                            conn.rollback()
+                            print(f"An error occurred: {e}")
                 if len(links) >= numberOfResultsToCrawl:
                     break
+        cursor.close()
+        conn.close()
         return links[:numberOfResultsToCrawl]
     except requests.RequestException as e:
         print(f"Request error: {e}")
@@ -35,6 +68,10 @@ def googleSearch(sourceName, cryptoName, numberOfResultsToCrawl):
     except Exception as e:
         print(f"Unexpected error: {e}")
         return {"error": "Unexpected error in google search scope"}
+
+# Calling googleSearch function by different threads
+def asyncGoogleSearch(sourceName, cryptoName, numberOfResultsToCrawl):
+    return executor.submit(googleSearch, sourceName, cryptoName, numberOfResultsToCrawl)
     
 @app.route('/crawl', methods=['GET'])
 def crawl():
@@ -42,7 +79,11 @@ def crawl():
         sourceName = request.args.get('sourceName')
         cryptoName = request.args.get('cryptoName')
         numberOfResultsToCrawl = request.args.get('number', 10, type=int)
-        results = googleSearch(sourceName, cryptoName, numberOfResultsToCrawl)
+        
+        #results = googleSearch(sourceName, cryptoName, numberOfResultsToCrawl)
+        future = asyncGoogleSearch(sourceName, cryptoName, numberOfResultsToCrawl)
+        results = future.result()
+        
         return jsonify({
                 "sourceName": sourceName,
                 "cryptoName": cryptoName,
@@ -53,6 +94,6 @@ def crawl():
         return {"error": "Unexpected error in crawling scope"}
 
 if __name__ == '__main__':
-    app.run(port=8000, debug=True)
+    app.run(port=8000, debug=True, threaded=True)
     # Example request:
     # http://127.0.0.1:8000/crawl?sourceName=nytimes.com&cryptoName=Bitcoin&number=30
